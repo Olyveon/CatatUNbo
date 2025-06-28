8ec0e663ac477335aec4c33fa46624307d8cb9e7
Olyveon <114949641+Olyveon@users.noreply.github.com>
2025-06-27 19:26:08 -0500

    added initial consult ui

--- Proyecto/SpyNet/src/main/java/org/catatunbo/spynet/App.java
+++ Proyecto/SpyNet/src/main/java/org/catatunbo/spynet/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("SpyNet");
         stage.setScene(scene);
         stage.show();
