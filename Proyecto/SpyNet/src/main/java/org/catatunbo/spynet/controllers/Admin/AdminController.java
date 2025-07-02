20d8efa769073b311673965caee16ceaba4cc4fc
Olyveon <ferojasm@unal.edu.co>
2025-07-01 19:23:55 -0500

    added a new panel and changed the functions of some buttons

--- Proyecto/SpyNet/src/main/java/org/catatunbo/spynet/controllers/Admin/AdminController.java
+++ Proyecto/SpyNet/src/main/java/org/catatunbo/spynet/controllers/Admin/AdminController.java
@@ -1,5 +1,28 @@
 package org.catatunbo.spynet.controllers.Admin;
 
-public class AdminController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class AdminController {
+    @FXML
+    private void handleExit(ActionEvent event) throws IOException {
+        // Load main.fxml
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = loader.load();
+
+        // Get the current stage
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set the new scene
+        Scene scene = new Scene(root, 1280, 800);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
