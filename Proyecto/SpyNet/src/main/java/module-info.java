e58a0db0902baa6a5a56a5968f548a6a86a281e8
Sarfito <vera60109@gmail.com>
2025-07-09 10:34:43 -0500

    Requires the sql library for database operations

--- Proyecto/SpyNet/src/main/java/module-info.java
+++ Proyecto/SpyNet/src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module org.catatunbo.spynet {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    requires transitive java.sql;
     opens org.catatunbo.spynet to javafx.fxml;
     exports org.catatunbo.spynet;
     opens org.catatunbo.spynet.controllers to javafx.fxml;
