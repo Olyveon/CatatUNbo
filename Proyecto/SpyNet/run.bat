8966b1fe7ecd79c1a7075e0e2f4912d023cc76ea
Sarfito <srojasfe@unal.edu.co>
2025-07-11 21:21:19 -0500

    Making run.bat executable

--- Proyecto/SpyNet/run.bat
+++ Proyecto/SpyNet/run.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0"
-echo Compilando proyecto...
-mvn clean compile
-if %errorlevel% neq 0 (
-    echo Error en la compilacion. Verifica los errores anteriores.
-    pause
-    exit /b 1
-)
-echo Ejecutando SpyNet...
+rem echo Compilando proyecto...
+rem mvn clean compile
+rem if %errorlevel% neq 0 (
+rem    echo Error en la compilacion. Verifica los errores anteriores.
+rem    pause
+rem    exit /b 1
+rem )
+rem echo Ejecutando SpyNet...
 mvn exec:java -Dexec.mainClass="org.catatunbo.spynet.App"
 pause
